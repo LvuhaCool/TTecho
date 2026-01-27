b19fe704d1693916c7405c8fb7f6a59720135395
Leo Gharbaruk <ghabaruk@gmail.com>
2026-01-27 17:06:31 +0300

    remove type module in html script tag and take data from data to script file

--- js/script.js
+++ js/script.js
@@ -1,6 +1,207 @@
 // Переменные
 // Массив карточек
-import { data } from "./data.js";
+const data = [
+    {
+        id: 0,
+        img: "./img/card-images/dignics-05.jpg",
+        alt: "Dignics 05",
+        name: "Butterfly Dignics 05",
+        price: "8000",
+        inactivePrice: "10000₽"
+    },
+    {
+        id: 1,
+        img: "./img/card-images/dignics-09c.webp",
+        alt: "Dignics 09c",
+        name: "Butterfly Dignics 09c",
+        price: "9000",
+        inactivePrice: "11000₽"
+    },
+    {
+        id: 2,
+        img: "./img/card-images/tenergy-05.png",
+        alt: "Tenergy 05",
+        name: "Butterfly Tenergy 05",
+        price: "7000",
+        inactivePrice: ""
+    },
+    {
+        id: 3,
+        img: "./img/card-images/tenergy-19.jfif",
+        alt: "Tenergy 19",
+        name: "Buttefly Tenergy 19",
+        price: "9000",
+        inactivePrice: "10000₽"
+    },
+    {
+        id: 4,
+        img: "./img/card-images/viscaria.jpg",
+        alt: "Viscaria",
+        name: "Butterfly Viscaria",
+        price: "15000",
+        inactivePrice: ""
+    },
+    {
+        id: 5,
+        img: "./img/card-images/tb-alc.jpg",
+        alt: "Timo Boll ALC",
+        name: "Butterfly Timo Boll ALC",
+        price: "20000",
+        inactivePrice: "21000₽"
+    },
+    {
+        id: 6,
+        img: "./img/card-images/tb-zlc.webp",
+        alt: "Timo Boll ZLC",
+        name: "Butterfly Timo Boll ZLC",
+        price: "21500",
+        inactivePrice: ""
+    },
+    {
+        id: 7,
+        img: "./img/card-images/feint-long-3.jpg",
+        alt: "Feint Long III",
+        name: "Butterfly Feint Long III",
+        price: "9000",
+        inactivePrice: ""
+    },
+    {
+        id: 8,
+        img: "./img/card-images/feint-long-2.jfif",
+        alt: "Feint Long II",
+        name: "Butterfly Feint Long II",
+        price: "8000",
+        inactivePrice: "8500₽"
+    },
+    {
+        id: 9,
+        img: "./img/card-images/mizuno-wave-drive-9-white.jfif",
+        alt: "Mizuno wave drive 9",
+        name: "Mizuno wave drive 9",
+        price: "12000",
+        inactivePrice: ""
+    },
+    {
+        id: 10,
+        img: "./img/card-images/mizuno-wave-drive-neo-3-black.jfif",
+        alt: "Mizuno wave drive neo 3",
+        name: "Mizuno wave drive neo 3",
+        price: "13000",
+        inactivePrice: "15000₽"
+    },
+    {
+        id: 11,
+        img: "./img/card-images/zyre-03.jpg",
+        alt: "Zyre 03",
+        name: "Butterfly Zyre 03",
+        price: "15000",
+        inactivePrice: ""
+    },
+    {
+        id: 12,
+        img: "./img/card-images/bleugrip-c1.jpg",
+        alt: "BlueGrip C1",
+        name: "Butterfly BlueGrip C1",
+        price: "8000",
+        inactivePrice: ""
+    },
+    {
+        id: 13,
+        img: "./img/card-images/bluegrip-c2.jpg",
+        alt: "BlueGrip C2",
+        name: "Butterfly BlueGrip C2",
+        price: "8000",
+        inactivePrice: ""
+    },
+    {
+        id: 14,
+        img: "./img/card-images/skachkov-carbon.jpg",
+        alt: "Skachkov Carbon",
+        name: "Donic Skachkov Carbon",
+        price: "6000",
+        inactivePrice: "6500₽"
+    },
+    {
+        id: 15,
+        img: "./img/card-images/bt-shoe.jfif",
+        alt: "Buttrfly Shoe",
+        name: "Buttrfly Shoe",
+        price: "15000",
+        inactivePrice: ""
+    },
+    {
+        id: 16,
+        img: "./img/card-images/tenergy-05fx.jpg",
+        alt: "Tenergy 05-FX",
+        name: "Butterfly Tenergy 05-FX",
+        price: "7500",
+        inactivePrice: ""
+    },
+    {
+        id: 17,
+        img: "./img/card-images/killer.jpg",
+        alt: "Killer",
+        name: "Killer шипы",
+        price: "4000",
+        inactivePrice: ""
+    },
+    {
+        id: 18,
+        img: "./img/card-images/doctor-ko.png",
+        alt: "Dr. Neubauer KO",
+        name: "Dr. Neubauer KO",
+        price: "6000",
+        inactivePrice: ""
+    },
+    {
+        id: 19,
+        img: "./img/card-images/petr-korbel.jfif",
+        alt: "Petr Korbel JP",
+        name: "Donic Petr Korbel JP",
+        price: "9000",
+        inactivePrice: ""
+    },
+    {
+        id: 20,
+        img: "./img/card-images/primorac.jpg",
+        alt: "Primorac",
+        name: "Donic Primorac",
+        price: "10000",
+        inactivePrice: ""
+    },
+    {
+        id: 21,
+        img: "./img/card-images/rozena.jpg",
+        alt: "Rozena",
+        name: "Butterfly Rozena",
+        price: "8000",
+        inactivePrice: ""
+    },
+    {
+        id: 22,
+        img: "./img/card-images/ovtcharov-carbon.jpg",
+        alt: "Ovtcharov Carbon",
+        name: "Donic Ovtcharov Carbon",
+        price: "12000",
+        inactivePrice: ""
+    },
+    {
+        id: 23,
+        img: "./img/card-images/donic-desto.jpg",
+        alt: "Donic Desto",
+        name: "Donic Desto",
+        price: "4500",
+        inactivePrice: ""
+    },
+    {
+        id: 24,
+        img: "./img/card-images/donic-waldner.jpg",
+        alt: "Donic Waldner",
+        name: "Donic Waldner",
+        price: "7500",
+        inactivePrice: ""
+    },
+];
 let shownCardsCount = 0;
 let showCardsStep = 12;
 // Определение мобильного устройтсва
