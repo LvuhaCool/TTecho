26d093bb3313b76bc6d67e64502ed966d1121f77
Leo Gharbaruk <ghabaruk@gmail.com>
2026-02-04 21:54:34 +0300

    another bug you know

--- js/script.js
+++ js/script.js
@@ -673,6 +673,7 @@ function showCardModal(event) {
 cardInfoHideBtn.addEventListener("click", hideCardInfo);
 function hideCardInfo() {
     inputHideToolTips();
+    hamburgerInput.value = "";
     document.body.classList.remove("card-none");
     htmlTag.classList.remove("card-none");
     cardInfo.classList.remove("visible-card-info");
